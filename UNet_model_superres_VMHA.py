2c40d5e20acc64f2f2b93c0fcdc514f48a2b73a4
Adriano Ettari <a.ettari@studenti.unina.it>
2024-09-13 15:32:10 +0200

    remove batch normalization

--- UNet_model_superres_VMHA.py
+++ UNet_model_superres_VMHA.py
@@ -232,18 +232,14 @@ class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        # self.batch_norm1 = nn.BatchNorm2d(out_channels) ############ TO TEST ############
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding)
-        # self.batch_norm2 = nn.BatchNorm2d(out_channels) ############ TO TEST ############
 
     def forward(self, x):
         residual = x
         out = self.conv1(x)
-        # out = self.batch_norm1(out) ############ TO TEST ############
         out = self.relu(out)
         out = self.conv2(out)
-        # out = self.batch_norm2(out) ############ TO TEST ############
         out += residual
         return out
 
